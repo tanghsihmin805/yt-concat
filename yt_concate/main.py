70f79fb023ee84cd5f21cf91daa12ed2cd2fd249
Tom Tang <tanghsihmin@gmail.com>
2021-04-02 02:09:54 +0800

    Refactor code into pipeline design pattern

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,44 +1,22 @@
-import urllib.request
-import json
-import os
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
 
-# 方法一
-# import sys
-# sys.path.append("..")  # 課程中使用絕對路徑call package未往上一層，但我目前無法成功呼叫，所以需要此行
-# from yt_concate.settings import API_KEY
-
-#方法二
-from settings import API_KEY
+from yt_concate.pipeline.pipeline import Pipeline
 
 CHANNEL_ID = 'UCFZPaQsh452W4_WDItGh8sw'
 
-print(API_KEY)
 
-def get_all_video_in_channel(channel_id):
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
 
-    first_url = base_search_url + \
-        'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(
-            API_KEY, channel_id)
+    steps = [
+        GetVideoList(),
+    ]
 
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-video_list = get_all_video_in_channel(CHANNEL_ID)
-print(video_list)
+if __name__ == '__main__':
+    main()
