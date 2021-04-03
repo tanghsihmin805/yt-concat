6a2f879fa600df947fc0d3dcd291e2429beccb8d
Tom Tang <tanghsihmin@gmail.com>
2021-04-03 23:51:21 +0800

    Successfully read captions into data structure

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,6 +1,7 @@
+from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
 from yt_concate.pipeline.steps.download_captions import DownloadCaptions
-from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.read_captions import ReadCaptions
 from yt_concate.pipeline.steps.postflight import Postflight
 from yt_concate.pipeline.steps.step import StepException
 from yt_concate.pipeline.pipeline import Pipeline
@@ -19,6 +20,7 @@ def main():
         Preflight(),
         GetVideoList(),
         DownloadCaptions(),
+        ReadCaptions(),
         Postflight(),
     ]
 
