907f340583f7b2ea52b7c059efaea6352c253995
Tom Tang <tanghsihmin@gmail.com>
2021-04-05 04:03:30 +0800

    Successfully search word from captions and create yt data model

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -11,28 +11,31 @@ from yt_concate.settings import CAPTIONS_DIR
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
-        for url in data:
-            if utils.caption_file_exist(url):
+        for yt in data:
+            print('downloading caption for', yt.id)
+            if utils.caption_file_exist(yt):
                 print('found existing caption file')
                 continue
-            print('downloading caption for', url)
+
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
             except AttributeError:
-                print('AttributError when downloading caption for', url)
+                print('AttributError when downloading caption for', yt.url)
                 continue
             except KeyError:
-                print('KeyError when downloading caption for', url)
+                print('KeyError when downloading caption for', yt.url)
                 continue
 
             # save the caption to a file named Output.txt
-            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file = open(utils.get_caption_filepath(yt.url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
 
         end = time.time()
         print('took', end - start, 'seconds')
+
+        return data
 
 
--- yt_concate/pipeline/steps/read_captions.py
+++ yt_concate/pipeline/steps/read_captions.py
@@ -1,16 +1,13 @@
-import os
-from pprint import pprint
-
 from .step import Step
-from yt_concate.settings import CAPTIONS_DIR
 
 
 class ReadCaptions(Step):
     def process(self, data, inputs, utils):
-        data = {}
-        for captions_file in os.listdir(CAPTIONS_DIR):
+        for yt in data:
+            if not utils.caption_file_exist(yt):
+                continue
             captions = {}
-            with open(os.path.join(CAPTIONS_DIR, captions_file),'r') as f:
+            with open(yt.caption_filepath,'r') as f:
                 time_line = False
                 time = None
                 caption = None
@@ -24,7 +21,6 @@ class ReadCaptions(Step):
                         caption = line
                         captions[caption] = time
                         time_line = False
-            data[captions_file] = captions
-        pprint(data)
+            yt.captions = captions
         return data
 
