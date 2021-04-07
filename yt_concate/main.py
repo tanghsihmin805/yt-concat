a6ac0ca3a5ef8ac44cc4112d6eb8f72a270d3af6
Tom Tang <tanghsihmin@gmail.com>
2021-04-07 19:01:35 +0800

    Finished editing video and producing output video file

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -2,6 +2,7 @@ from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
 from yt_concate.pipeline.steps.initialize_yt import InitializeYT
 from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.edit_video import EditVideo
 from yt_concate.pipeline.steps.read_captions import ReadCaptions
 from yt_concate.pipeline.steps.search import Search
 from yt_concate.pipeline.steps.download_videos import DownloadVideos
@@ -18,6 +19,7 @@ def main():
     inputs = {
         'channel_id': CHANNEL_ID,
         'search_word': "incredible",
+        'limit': 20
     }
 
     steps = [
@@ -28,6 +30,7 @@ def main():
         ReadCaptions(),
         Search(),
         DownloadVideos(),
+        EditVideo(),
         Postflight(),
     ]
 
--- yt_concate/utils.py
+++ yt_concate/utils.py
@@ -3,6 +3,7 @@ import os
 from yt_concate.settings import DOWNLOADS_DIR
 from yt_concate.settings import VIDEOS_DIR
 from yt_concate.settings import CAPTIONS_DIR
+from yt_concate.settings import OUTPUT_DIR
 
 
 class Utils():
@@ -13,6 +14,7 @@ class Utils():
         os.makedirs(DOWNLOADS_DIR, exist_ok=True)
         os.makedirs(VIDEOS_DIR, exist_ok=True)
         os.makedirs(CAPTIONS_DIR, exist_ok=True)
+        os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     def get_video_list_filepath(self, channel_id):
         return os.path.join(DOWNLOADS_DIR, channel_id + '.txt')
@@ -28,3 +30,7 @@ class Utils():
     def video_file_exist(self, yt):
         filepath = yt.video_filepath
         return os.path.exists(filepath) and os.path.getsize(filepath) > 0
+
+    def get_output_filepath(self, channel_id , search_word):
+        filename = channel_id + '_' + search_word + '.mp4'
+        return os.path.join(OUTPUT_DIR, filename)
